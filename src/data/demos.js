b073fb3821f4d805b610622d466d96db1ca83a00
Mauro Vicens <blackeagledhs@gmail.com>
2026-05-12 10:59:21 -0300

    add new demo portfolio

--- src/data/demos.js
+++ src/data/demos.js
@@ -1,13 +1,20 @@
 export const demos = [
   {
-    key: "saas",
+    key: "landing",
     image: "/demos/saas-landing-page.png",
     url: "https://black-eagle-tech.github.io/saas-landing-page/",
+    badge: "Popular"
   },
-
   {
-    key: "localbusiness",
+    key: "localBusiness",
     image: "/demos/local-business.png",
     url: "https://black-eagle-tech.github.io/local-business/",
+    badge: "Business"
   },
+  {
+    key: "portfolio",
+    image: "/demos/portfolio.png",
+    url: "https://black-eagle-tech.github.io/portfolio/#/",
+    badge: "Personal"
+  }
 ]
